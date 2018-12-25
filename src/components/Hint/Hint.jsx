687b90b07f441a117957b199fdb0a4b0cf5f11b8
Zakhar Ovcharov <scotkrauz@gmail.com>
2018-12-25 23:24:44 +0300

    added styled component

--- src/components/Hint/Hint.jsx
+++ src/components/Hint/Hint.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import './hint.scss'
+const StyledHint = styled.div`
+  display: inline-block;
+  background-color: rgba(0, 0, 0, 0.75);
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+import './hint.scss' 
 
 export default function Hint (props) {
   return (
-    <div className="b-hint">
+    <StyledHint>
       {props.innerText}
-    </div>
+    </StyledHint>
   )
 }
---
+++ src/utils/calculatePosition.js
@@ -0,0 +1,10 @@
+export function calculatePosition (el, position) {
+  switch(position) {
+    case 'bottom':
+      return calculatePositionBottom(el)
+  }
+}
+
+function calculatePositionBottom (el) {
+
+}
